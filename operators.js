1d969c52311fca8e6b295138d385a4ccad9a52ea
vaishu024 <vaishnavisuresh024@gmail.com>
2024-01-11 12:10:48 +0530

    initial commit

---
+++ operators.js
@@ -0,0 +1,29 @@
+let a=20
+let b=10
+let B="20"
+let c=a+b
+console.log(c)
+var d=a-b
+console.log(d)
+var e=a*b
+console.log(e)
+var f=a/b
+console.log(f)
+var g=a%b
+console.log(g)
+var h=a**b
+console.log(h)
+a++
+console.log(a)
+b--
+console.log(b)
+console.log(a==b)
+console.log(a==B)
+console.log(a===B)
+let o=true
+let p=false
+console.log(o&&p)
+console.log(o||p)
+console.log(!o)
+console.log(!p)
+
